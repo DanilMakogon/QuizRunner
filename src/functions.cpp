24c327557de5a238579fe5564031c85064619211
DanilMakogon <flanterz@yandex.ru>
2017-04-30 16:24:30 +0700

    ReadKey func declaration# Пожалуйста, введите сообщение коммита для ваших изменений. Строки,# начинающиеся с «#» будут проигнорированы, а пустое сообщение# отменяет процесс коммита.# На ветке dev# Изменения, которые будут включены в коммит:#	изменено:      src/functions.cpp#

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <ncurses.h>
 
 using namespace std;
 
@@ -17,5 +18,31 @@ int readBase(char *filename) {
 		if (ist.eof())
 			break;
 	}
-	return 0;
+	return dataBase.size();
+}
+
+int readKey() {
+	initscr();
+	echo();
+	char c;
+	scanf("%c", &c);
+	switch(c) {
+		case 'a': case 'A': case '1': {
+			return 1;
+			break;
+		}
+		case 'b': case 'B': case '2': {
+			return 2;
+			break;
+		}
+		case 'c': case 'C': case '3': {
+			return 3;
+			break;
+		}
+		case 'd': case 'D': case '4': {
+			return 4;
+			break;
+		}
+	}
+	return -1;
 }
