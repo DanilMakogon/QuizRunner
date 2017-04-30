fc5ab827b84268a15959e0a277ae09dc39e9c0b4
DanilMakogon <flanterz@yandex.ru>
2017-04-30 12:51:38 +0700

    Добавлена структура базы

--- headers/functions.h
+++ headers/functions.h
@@ -1,4 +1,19 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <string>
+using namespace std;
+
+struct base {
+	string ques;
+	string ans1;
+	string ans2;
+	string ans3;
+	string ans4;
+	short right_ans;
+};
+
+int readBase(char *filename);
+
+
 #endif
